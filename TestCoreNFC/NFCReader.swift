818b9c184bf69f442c4ad3cf0b1d4427ab22feea
Bomi <bomiishere@gmail.com>
2018-01-04 17:13:49 +0800

    detect NFC tag by using VYNFCKit

--- TestCoreNFC/NFCReader.swift
+++ TestCoreNFC/NFCReader.swift
@@ -8,31 +8,86 @@
 
 import Foundation
 import CoreNFC
+import VYNFCKit
 
 class NFCReader: NSObject , NFCNDEFReaderSessionDelegate {
     
     func readerSession(_ session: NFCNDEFReaderSession, didDetectNDEFs messages: [NFCNDEFMessage]) {
+        
         for message in messages {
-            var result = ""
-            for record in message.records {
-
-                print("New Record Found:")
+            for payload in message.records {
+                guard let parsedPayload = VYNFCNDEFPayloadParser.parse(payload) else {
+                    continue
+                }
                 
+                var text = ""
+                var urlString = ""
                 
-//                result += String.init(data: record.payload.advanced(by: 3), encoding: .utf8)! // 1
-//
-//                print(NSString(data: record.identifier, encoding: 0))
+                if let parsedPayload = parsedPayload as? VYNFCNDEFTextPayload {
+                    
+                    text = "[Text payload]\n"
+                    text = String(format: "%@%@", text, parsedPayload.text)
+                    
+                } else if let parsedPayload = parsedPayload as? VYNFCNDEFURIPayload {
+                    
+                    text = "[URI payload]\n"
+                    text = String(format: "%@%@", text, parsedPayload.uriString)
+                    urlString = parsedPayload.uriString
+                    
+                } else if let parsedPayload = parsedPayload as? VYNFCNDEFTextXVCardPayload {
+                    
+                    text = "[TextXVCard payload]\n"
+                    text = String(format: "%@%@", text, parsedPayload.text)
+                    
+                } else if let sp = parsedPayload as? VYNFCNDEFSmartPosterPayload {
+                    
+                    text = "[SmartPoster payload]\n"
+                    for textPayload in sp.payloadTexts {
+                        if let textPayload = textPayload as? VYNFCNDEFTextPayload {
+                            text = String(format: "%@%@\n", text, textPayload.text)
+                        }
+                    }
+                    text = String(format: "%@%@", text, sp.payloadURI.uriString)
+                    urlString = sp.payloadURI.uriString
+                    
+                } else if let wifi = parsedPayload as? VYNFCNDEFWifiSimpleConfigPayload {
+                    
+                    for case let credential as VYNFCNDEFWifiSimpleConfigCredential in wifi.credentials {
+                        text = String(format: "%@SSID: %@\nPassword: %@\nMac Address: %@\nAuth Type: %@\nEncrypt Type: %@",
+                                      text, credential.ssid, credential.networkKey, credential.macAddress,
+                                      VYNFCNDEFWifiSimpleConfigCredential.authTypeString(credential.authType),
+                                      VYNFCNDEFWifiSimpleConfigCredential.encryptTypeString(credential.encryptType)
+                        )
+                    }
+                    if let version2 = wifi.version2 {
+                        text = String(format: "%@\nVersion2: %@", text, version2.version)
+                    }
+                    
+                } else {
+                    
+                    text = "Parsed but unhandled payload type"
+                    
+                }
                 
-                print(String(data: record.payload, encoding: .utf8))
-//                print(NSString(data: record.type, encoding: 0))
-//                print(record.typeNameFormat)
+                NSLog("%@", text)
+                
+                DispatchQueue.main.async {
+                    if urlString != "" {
+                        // Close NFC reader session and open URI after delay. Not all can be opened on iOS.
+                        session.invalidate()
+                        if let url = URL(string: urlString) {
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 1, execute: {
+                                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                            })
+                        }
+                    }
+                }
+                
             }
-//            print(result)
         }
     }
     
     func readerSession(_ session: NFCNDEFReaderSession, didInvalidateWithError error: Error) {
-        print("NFC NDEF Invalidated")
         print("\(error.localizedDescription)")
     }
     
